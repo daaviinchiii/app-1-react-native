4f1818f2f8c5a998d4f18084268450ba3d0e4c78
daaviinchiii <85748800+daaviinchiii@users.noreply.github.com>
2024-08-23 15:04:41 +0000

    aula 23

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,10 +1,27 @@
-import {styles} from './styles';
-import { Text, View } from "react-native"
-export function Home(){
-  return(
+import { styles } from "./styles";
+import { Text, TextInput, View,TouchableOpacity } from "react-native";
+export function Home() {
+  function handlePraticipantAdd(){
+    console.log("clicou")
+  }
+  return (
     <View style={styles.container}>
-<Text style={styles.eventName}>Nome do evento</Text>
-<Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
-</View>
-  )
+      <Text style={styles.eventName}>
+        Nome do evento
+        </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022
+        </Text>
+        <View style={styles.form}>
+      <TextInput 
+      style={styles.imput} 
+      placeholder="Nome do participante"
+      placeholderTextColor="#6b6b6b"
+      />
+      <TouchableOpacity style={styles.button} onPress={handlePraticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
